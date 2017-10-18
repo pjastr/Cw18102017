b3f1b543f6165295085ad2e68ed30fc8d3a56eeb
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-18 18:31:31 +0200

    Add files via upload

---
+++ ConsoleApplication25/ConsoleApplication25/Matematyka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication25
+{
+        
+    
+    class Matematyka
+    {
+        private static double pi = 3.14;
+
+        public static double PoleKola(double r)
+        {
+            return pi * r *r;
+        }
+    }
+}
